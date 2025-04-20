9932adbb361b231fd171ef27dda5a0f9a16de9d7
GUIZ4RD <gfgullo@gmail.com>
2025-04-20 19:51:46 +0200

    added image model

--- coicoi/models/__init__.py
+++ coicoi/models/__init__.py
@@ -1,5 +1,6 @@
 from .model import Model
 from .multi_model import MultiModel
 from .collaborative_model import CollaborativeModel
+from .image_model import ImageModel
 
-__all__ = ['Model', 'MultiModel', 'CollaborativeModel'] 
+__all__ = ['Model', 'MultiModel', 'CollaborativeModel', 'ImageModel'] 
